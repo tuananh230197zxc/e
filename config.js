cd6d5bf9500175292250a35b9592aece02b301db
tuananh230197zxc <88013981+tuananh230197zxc@users.noreply.github.com>
2023-03-28 22:27:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Em';
-const giftUrl = 'http://nodemy.vn';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100014680295356';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
